f766f2f28cad9e233b5e1ea7e00d27b06e0acaf8
Sergio Arbeo <serabe@serabe.com>
2011-02-07 20:17:06 +0100

    Refactoring and history added.

--- src/little_numbers.clj
+++ src/little_numbers.clj
@@ -1,9 +1,8 @@
 (ns little-numbers)
 
 (defn to4 [x]
-  (let [s (count x)
-        a (- 4 s)]
-    (str (apply str (repeat a "0")) x)))
+  (let [s (str x)]
+    (str (apply str (repeat (- 4 (count s)) "0")) s)))
 
 (defn has-repeated-cipher?
   [x]
@@ -13,7 +12,13 @@
   [x]
   (filter #(not (has-repeated-cipher? %)) x))
 
-(def *seq* (ref (filter-fn (map #(to4 (str %)) (range 123 9999)))))
+(def possible-values (filter #(not (has-repeated-cipher? %)) (map to4 (range 123 9877))))
+
+(def *seq* (ref []))
+
+(defn reset-game []
+  (dosync
+   (ref-set *seq* [])))
 
 (defn get-dead
   [number guess]
@@ -37,24 +42,39 @@
      :hurt (get-hurt number guess dead)}))
 
 (defn satisfy-guess
-  [number guess score]
-  (let [sc (get-score guess number)
-        dn (:dead sc)
-        hn (:hurt sc)
-        dg (:dead score)
-        hg (:hurt score)]
+  [number guess {dg :dead hg :hurt}]
+  (let [{dn :dead hn :hurt} (get-score guess number)]
     (and (= dn dg)
          (= hn hg))))
 
-(defn filter-guess
+(defn next-guess []
+  (if (empty? @*seq*)
+    {:guess "0123" :count 5040}
+    (loop [[{guess :guess score :score} & xs] @*seq*
+           possible (filter #(satisfy-guess % guess score) possible-values)]
+      (let [filtered (filter #(satisfy-guess % guess score) possible)]
+        (if (empty? xs)
+          {:guess (first filtered) :count (count filtered)}
+          (recur xs filtered))))))
+
+(defn add-guess-to-ref
   [guess score]
-  (fn [x]
-    (filter #(satisfy-guess % guess score) x)))
+  (fn [last-val]
+    (conj last-val {:guess guess
+                    :score score})))
 
 (defn add-guess
   ([guess score]
      (dosync
-      (alter *seq* (filter-guess guess score))
-      (first @*seq*)))
+      (alter *seq* conj {:guess guess :score score})
+      (next-guess)))
   ([guess dead hurt]
      (add-guess guess {:dead dead :hurt hurt})))
+
+(defn guesses-needed-for [number]
+  (reset-game)
+  (loop [{guess :guess count :count} (next-guess)
+         acc   1]
+    (if (= 1 count)
+      acc
+      (recur (add-guess guess (get-score number guess)) (inc acc)))))
